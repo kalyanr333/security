816121f8cfa53136e4a28e216878cd3c5961020e
kalyanr333 <radhika.kalyan03@gmail.com>
2024-10-18 13:45:15 -0500

    Update print_pwd.py

--- print_pwd.py
+++ print_pwd.py
@@ -7,6 +7,3 @@ def insecure_function(password):
 user_input = "sensitivePassword"
 insecure_function(user_input)
 
-# (CWE-359)
-# This sample Python file contains a function that prints a password to the console without any security measures.
-# It can be used to test SAST tools' ability to identify sensitive information exposure.
