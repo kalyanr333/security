ce252ee5d1f3c48b888fa261ecf248bbdd5d9f1c
kalyanr333 <radhika.kalyan03@gmail.com>
2024-10-18 08:43:35 -0500

    Update print_pwd.py

--- print_pwd.py
+++ print_pwd.py
@@ -1,8 +1,8 @@
 import os
 
 def insecure_function(password):
-    #print("Received password: " + password)
-    print("Received password: " + base64.b64encode(password..encode("utf-8")))
+    print("Received password: " + password)
+    #print("Received password: " + base64.b64encode(password..encode("utf-8")))
 
 user_input = "sensitivePassword"
 insecure_function(user_input)
